9a3ad3be7102dffb8d62f1fc5851187396aa7a66
Eeo Jun <141bytes@gmail.com>
2016-09-03 11:14:25 +0800

    change to buffer pool implementation

--- commands/server.go
+++ commands/server.go
@@ -1,13 +1,29 @@
 package commands
 
 import (
+	"os"
+	"path/filepath"
 	"github.com/gin-gonic/gin"
 	"github.com/eugene-eeo/psync/lib"
+	"github.com/oxtoacart/bpool"
 )
 
 func Serve(addr string) {
+	root := lib.PsyncBlocksDir()
+	pool := bpool.NewBytePool(20, lib.BLOCK_SIZE)
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	r.Static("/", lib.PsyncBlocksDir())
+	r.GET("/:checksum", func(c *gin.Context) {
+		checksum := c.Param("checksum")
+		f, err := os.Open(filepath.Join(root, checksum))
+		if err != nil {
+			c.AbortWithStatus(404)
+		}
+		defer f.Close()
+		buff := pool.Get()
+		f.Read(buff)
+		c.Writer.Write(buff)
+		pool.Put(buff)
+	})
 	r.Run(addr)
 }
